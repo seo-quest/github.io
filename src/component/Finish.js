8591d69e83e6196f257e8d407539aff1747ea831
LeeSangJun <shaamy@naver.com>
2021-09-15 02:00:44 +0900

    improve design

--- src/component/Finish.js
+++ src/component/Finish.js
@@ -1,14 +1,22 @@
 import React from "react";
+import { Button } from "@material-ui/core";
+import PhoneAndroidIcon from "@material-ui/icons/PhoneAndroid";
 
 function Finish() {
+  const callToHim = () => {
+    window.location.href = "tel:010-5063-3697";
+  };
   return (
     <div className="mission">
       <h1 className="mission-title">모든 미션이 완료되었습니다.</h1>
       <h2 className="mission-description">
         이제 당신을 애타게 기다리는 사람에게 전화를 걸어주세요.
       </h2>
-      <div className="mission-list-label">
-        <a href="tel:010-5063-3697"> 전화걸기 </a>
+      <div className="button-container">
+        <Button variant="contained" color="primary" onClick={callToHim}>
+          <PhoneAndroidIcon />
+          그에게 전화걸기
+        </Button>
       </div>
     </div>
   );
--- src/component/Mission_09.js
+++ src/component/Mission_09.js
@@ -16,9 +16,12 @@ function Mission_9() {
         아래 지도에 표시된 위치에서 버스를 타고,
         <br /> '남산타워 정류장' 에서 내려(3정거장) 남산타워까지 와주세요.
       </div>
+      <div className="image-container">
+        <img className="image" src="/bus_stop.png" alt="버스정류장" />
+      </div>
       <div className="button-container">
         <Button variant="contained" color="primary" onClick={handleNext}>
-          남산타워 앞이에요.
+          남산타워 앞 이에요
         </Button>
       </div>
     </div>
