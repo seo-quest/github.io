52a7cab00cd4065c5c2fd21a4f4e5a42de28c3d5
LeeSangJun <shaamy@naver.com>
2021-09-15 03:07:43 +0900

    update case

--- src/component/Mission_02.js
+++ src/component/Mission_02.js
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Button } from "@material-ui/core";
 import { useHistory } from "react-router-dom";
+import axios from "axios";
 
 function Mission_02() {
   const history = useHistory();
   const handleNext = () => {
     history.push("/Question/02");
   };
+  const [cabinetId, setCabinetId] = useState("");
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://raw.githubusercontent.com/seo-quest/seo-quest.github.io/master/userData.json"
+      )
+      .then((resp) => {
+        setCabinetId(resp.data.cabinetId);
+      });
+  }, []);
 
   return (
     <div className="mission">
@@ -15,7 +27,7 @@ function Mission_02() {
         그 곳에서 2번째로 가까운 백화점은 어디일까요?
       </h2>
       <div className="mission-text">
-        해당 백화점 2층 A 물품 보관함으로 가주세요.
+        해당 백화점 2층 {cabinetId} 물품 보관함으로 가주세요.
       </div>
       <div className="image-container">
         <img className="image" src="/cabinet_sample.png" alt="물품보관함" />
