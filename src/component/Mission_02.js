2a5a74d668de12893003a23eacdf7e8aed45ad3f
LeeSangJun <shaamy@naver.com>
2021-09-15 03:21:58 +0900

    ajax로 데이터 받아오도록 수정

--- src/component/Mission_02.js
+++ src/component/Mission_02.js
@@ -5,10 +5,7 @@ import axios from "axios";
 
 function Mission_02() {
   const history = useHistory();
-  const handleNext = () => {
-    history.push("/Question/02");
-  };
-  const [cabinetId, setCabinetId] = useState("");
+  const [lockerId, setLockerId] = useState("");
 
   useEffect(() => {
     axios
@@ -16,10 +13,13 @@ function Mission_02() {
         `https://raw.githubusercontent.com/seo-quest/seo-quest.github.io/master/userData.json?timestamp=${new Date().getTime()}`
       )
       .then((resp) => {
-        console.dir(resp.data);
-        setCabinetId(resp.data.cabinetId);
+        setLockerId(resp.data.lockerId_01);
       });
   }, []);
+
+  const handleNext = () => {
+    history.push("/Question/02");
+  };
 
   return (
     <div className="mission">
@@ -28,7 +28,7 @@ function Mission_02() {
         그 곳에서 2번째로 가까운 백화점은 어디일까요?
       </h2>
       <div className="mission-text">
-        해당 백화점 2층 {cabinetId} 물품 보관함으로 가주세요.
+        해당 백화점 2층 물품 보관함으로 가주세요. ({lockerId})
       </div>
       <div className="image-container">
         <img className="image" src="/cabinet_sample.png" alt="물품보관함" />
--- src/component/Mission_06.js
+++ src/component/Mission_06.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Button } from "@material-ui/core";
 import { useHistory } from "react-router-dom";
+import axios from "axios";
 
 function Mission_06() {
   const history = useHistory();
+  const [lockerId, setLockerId] = useState("");
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://raw.githubusercontent.com/seo-quest/seo-quest.github.io/master/userData.json?timestamp=${new Date().getTime()}`
+      )
+      .then((resp) => {
+        setLockerId(resp.data.lockerId_03);
+      });
+  }, []);
+
   const handleNext = () => {
     history.push("/Question/05");
   };
@@ -11,7 +24,9 @@ function Mission_06() {
   return (
     <div className="mission">
       <h1 className="mission-title">미션 :</h1>
-      <h2 className="mission-description">물품 보관함 번호는 XX입니다.</h2>
+      <h2 className="mission-description">
+        물품 보관함 번호는 {lockerId}입니다.
+      </h2>
       <div className="mission-text">
         이전 문제의 정답을 비밀번호로 보관함을 열어보세요!
       </div>
